ed8c62c6df1b522d366a16dc4ebce9fb23f27fb7
autodocs <autodocs>
2019-02-04 22:49:09 +0000

    build based on 00a46f8

--- dev/search_index.js
+++ dev/search_index.js
@@ -93,7 +93,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Basic Guide",
     "title": "A Basic Guide",
     "category": "section",
-    "text": "To use safer integers within your computations, where you have been using     explict digit sequences put them inside the safe integer constructors,     SafeInt(11) or SafeUInt(0x015A) and similarly for the bitsize-named versions     SafeInt8, SafeInt16 .. SafeInt128 and SafeUInt8 .. SafeUInt128   Where you had usedInt or UInt now use SafeInt or SafeUInt and similarly with the bitsize-named versions.    SafeInt and SafeUInt give you these arithmetic operators:     +, -, *, div, rem, fld, mod, ^     which have become overflow and underflow aware.The Int and UInt types can fail at simple arithmetic         and will continue carrying the incorrectness forward.     The validity of values obtained is difficult to ascertain.Most calculations proceed without incident,  and when used SafeInts operate as Ints should a calculation encouter an overflow or underflow,      we are alerted and the calculation does not proceed."
+    "text": "To use safer integers within your computations, where you have been using     explict digit sequences put them inside the safe integer constructors,     SafeInt(11) or SafeUInt(0x015A) and similarly for the bitsize-named versions     SafeInt8, SafeInt16 .. SafeInt128 and SafeUInt8 .. SafeUInt128   Where you had usedInt or UInt now use SafeInt or SafeUInt and similarly with the bitsize-named versions.    SafeInt and SafeUInt give you these arithmetic operators:     +, -, *, div, rem, fld, mod, ^     which have become overflow and underflow aware.The Int and UInt types can fail at simple arithmetic and will continue carrying the incorrectness forward.     So, the validity of values obtained is difficult to ascertain.Most calculations proceed without incident,  and when used SafeInts operate as Ints should a calculation encouter an overflow or underflow,      we are alerted and the calculation does not proceed."
 },
 
 {
@@ -205,7 +205,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Benchmarks",
     "title": "Benchmarks",
     "category": "section",
-    "text": "Relative to the system Integer types, using the Safer Integer types has a time cost of 1.25x..2.00x."
+    "text": "Relative to the system Integer types, using the Safer Integer types has a time cost of 1.10x..1.25x..2.00x."
 },
 
 {
