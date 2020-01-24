d0b82dfc087359b69a5393d2a9bc4be1f039635d
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-01-23 19:04:24 -0500

    fixup

--- src/binary_ops.jl
+++ src/binary_ops.jl
@@ -89,28 +89,28 @@ for OP in (:(>>>), :(>>), :(<<))
 
         @inline function $OP(x::S, y::Int) where {S<:SafeInteger}
             xx = baseint(x)
-            bitsof(T1) < abs(y) && throw(OverflowError("cannot shift $S by $y"))
+            bitsof(S) < abs(y) && throw(OverflowError("cannot shift $S by $y"))
             return reinterpret(S, $OP(xx, y))
         end
 
         @inline function $OP(x::I, y::S) where {I<:Signed, S<:SafeInteger}
             yy = baseint(y)
-            bitsof(T1) < abs(yy) && throw(OverflowError("cannot shift $I by $yy"))
+            bitsof(I) < abs(yy) && throw(OverflowError("cannot shift $I by $yy"))
             return reinterpret(I, $OP(x, yy))
         end
         @inline function $OP(x::S, y::I) where {S<:SafeInteger, I<:Signed}
             xx = baseint(x)
-            bitsof(T1) < abs(y) && throw(OverflowError("cannot shift $S by $y"))
+            bitsof(S) < abs(y) && throw(OverflowError("cannot shift $S by $y"))
             return reinterpret(S, $OP(xx, y))
         end
         @inline function $OP(x::I, y::S) where {I<:Unsigned, S<:SafeInteger}
             yy = baseint(y)
-            bitsof(T1) < abs(yy) && throw(OverflowError("cannot shift $I by $yy"))
+            bitsof(I) < abs(yy) && throw(OverflowError("cannot shift $I by $yy"))
             return reinterpret(I, $OP(x, yy))
         end
         @inline function $OP(x::S, y::I) where {S<:SafeInteger, I<:Unsigned}
             xx = baseint(x)
-            bitsof(T1) < abs(y) && throw(OverflowError("cannot shift $S by $y"))
+            bitsof(S) < abs(y) && throw(OverflowError("cannot shift $S by $y"))
             return reinterpret(S, $OP(xx, y))
         end
 
