e3f2168b704280132e6d69357a770a40735eb002
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-02-06 07:42:11 -0500

    Update compliance.jl

--- test/compliance.jl
+++ test/compliance.jl
@@ -1,3 +1,135 @@
+# (result::Integer, isoverflow::Bool) = op_with_overflow(x::I, y::I) where {I<:Integer}
+using Base.Checked: add_with_overflow, sub_with_overflow, mul_with_overflow
+
+
+# ----------------------------
+
+# for Unsigned Integer Types
+
+
+UT  = UInt128
+U1  = one(UT)
+UB  = 128
+UBH = UB >> 1
+
+const UBITS = UT(UB)
+const HALFUBITS = UT(UBH)
+
+const ULSBS  = (U1 << HALFUBITS) - U1
+
+
+function u128mul_ovf(x::UT, y::UT)
+   x_hi = x >> HALFUBITS
+   x_lo = x &  ULSBS
+   y_hi = y >> HALFUBITS
+   y_lo = y &  ULSBS
+
+   lowbits = x_lo * y_lo
+
+   x_hi_z = iszero(x_hi)
+   y_hi_z = iszero(y_hi)
+   x_hi_z && y_hi_z && return lowbits, false
+
+   ovf = !(x_hi_z || y_hi_z)
+
+   midbits1 = x_lo * y_hi
+   midbits2 = x_hi * y_lo
+   midbits  = midbits1 + midbits2
+   ovf = ovf || midbits < midbits1 || midbits > ULSBS
+
+   product = lowbits + (midbits << HALFUBITS)
+   ovf = ovf || product < lowbits
+
+   return product, ovf
+end
+
+
+UT8  = UInt8
+U81  = one(UT8)
+UB8  = 8
+UB8H = UB8 >> 1
+
+const U8BITS = UT(UB8)
+const HALFU8BITS = UT(UB8H)
+
+const U8LSBS  = (U81 << HALFU8BITS) - U81
+
+
+function u8mul_ovf(x::UT8, y::UT8)
+   x_hi = x >> HALFU8BITS
+   x_lo = x &  U8LSBS
+   y_hi = y >> HALFU8BITS
+   y_lo = y &  U8LSBS
+
+   lowbits = x_lo * y_lo
+
+   x_hi_z = iszero(x_hi)
+   y_hi_z = iszero(y_hi)
+   x_hi_z && y_hi_z && return lowbits, false
+
+   ovf = !(x_hi_z || y_hi_z)
+
+   midbits1 = x_lo * y_hi
+   midbits2 = x_hi * y_lo
+   midbits  = midbits1 + midbits2
+   ovf = ovf || midbits < midbits1 || midbits > U8LSBS
+
+   product = lowbits + (midbits << HALFU8BITS)
+   ovf = ovf || product < lowbits
+
+   return product, ovf
+end
+
+
+u8s = collect(UInt8(0):UInt8(255));
+
+for a in u8s
+   for b in u8s
+      ab1, ovf1 = mul_with_overflow(a, b)
+      ab2, ovf2 = u8mul_ovf(a, b)
+      if ovf1 != ovf2
+         println(string("a,b = ",a,", ",b,"    ovf1,ovf2 = ",ovf1,", ",ovf2))
+         break;break
+      end
+   end
+end
+
+#=
+ 
+const LONG_BIT      = UInt128(128)
+const HALF_LONG_BIT = UInt128(64)
+const HALFSIZE_MAX = (one(UInt128) << HALF_LONG_BIT) - one(UInt128)
+
+
+function umul_ovf(x::UInt128, y::UInt128)
+   x_hi = x >> HALF_LONG_BIT
+   x_lo = x &  HALFSIZE_MAX
+   y_hi = y >> HALF_LONG_BIT
+   y_lo = y &  HALFSIZE_MAX
+
+   lowbits = x_lo * y_lo
+
+   x_hi_z = iszero(x_hi)
+   y_hi_z = iszero(y_hi)
+   x_hi_z && y_hi_z && return lowbits, false
+
+   ovf = !(x_hi_z || y_hi_z)
+
+   midbits1 = x_lo * y_hi
+   midbits2 = x_hi * y_lo
+   midbits  = midbits1 + midbits2
+   ovf = ovf || midbits < midbits1 || midbits > HALFSIZE_MAX
+
+   product = lowbits + (midbits << HALF_LONG_BIT)
+   ovf = ovf || product < lowbits
+
+   return product, ovf
+end
+
+
+# -----------------------------
+
+
 # negative of the absolute value of x
 function negabs(x::I) where {I<:Base.Checked.SignedInt}
    signbit(x) ? x : -x
@@ -63,3 +195,5 @@ for r = 1:typemax(UInt8)
     uovf[r,c] = ovf
   end
 end
+
+=#
